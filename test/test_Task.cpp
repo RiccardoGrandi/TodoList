96e72e42df6136bd59063c7b8724d65ad3688cc5
RiccardoGrandi <riccardo.grandi@edu.unifi.it>
2025-09-04 11:24:54 +0200

    Add googletest e cartella test

---
+++ test/test_Task.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Riccardo on 04/09/2025.
+//
+
+#include <gtest/gtest.h>
+#include "./Task.h"
+#include "./ToDoList.h"
+
+TEST(TaskTest, TaskStartNotCompleted) {
+    Task t("Comprare il latte");
+    EXPECT_FALSE(t.isCompleted());
+}
+
+TEST(TaskTest, GetNameReturnCorrectName) {
+    Task t("Leggere un libro");
+    EXPECT_EQ(t.getName(), "Leggere un libro");
+}
+
+TEST(TaskTest, MarkCompletedSetsFlagTrue) {
+    Task t("Fare la spesa");
+    t.markCompleted();
+    EXPECT_TRUE(t.isCompleted());
+}
+
---
+++ test/test_ToDoList.cpp
@@ -0,0 +1,63 @@
+//
+// Created by Riccardo on 04/09/2025.
+//
+
+#include <gtest/gtest.h>
+#include "Task.h"
+#include "ToDoList.h"
+
+TEST(ToDoListTest, StartsEmpty) {
+    ToDoList list;
+    EXPECT_EQ(list.getTastCount(), 0);
+}
+
+TEST(ToDoListTest, AddTaskIncreasesCount) {
+    ToDoList list;
+    list.addTask("Pagare bollette");
+    EXPECT_EQ(list.getTastCount(), 0);
+}
+
+TEST(ToDoListTest, AddedTaskHasCorrectName) {
+    ToDoList list;
+    list.addTask("Andare dal dottore");
+    EXPECT_EQ(list.getTastCount(), 0);
+}
+
+TEST(ToDoListTest, CompleteTaskMarksItCompleted) {
+    ToDoList list;
+    list.addTask("Fare sport");
+    list.completeTask(0);
+    EXPECT_TRUE(list.getTask(0).isCompleted());
+}
+
+TEST(ToDoListTest, CompleteTaskOutOfBoundsDoesNothing) {
+    ToDoList list;
+    list.addTask("Andare al m");
+    list.completeTask(5); // indice non valido
+    EXPECT_FALSE(list.getTask(0).isCompleted());
+}
+
+TEST(ToDoListTest, GetTaskThrowsWhenOutOfBounds) {
+    ToDoList list;
+    list.addTask("Provare eccezione");
+    EXPECT_THROW(list.getTask(10), std::out_of_range);
+}
+
+TEST(ToDoListTest, MultipleTasksAdded) {
+    ToDoList list;
+    list.addTask("Task1");
+    list.addTask("Task2");
+    list.addTask("Task3");
+    EXPECT_EQ(list.getTaskCount(), 3);
+    EXPECT_EQ(list.getTask(1).getName(), "Task2");
+    EXPECT_FALSE(list.getTask(2).isCompleted());
+}
+
+TEST(ToDoListTest, ShowTasksRunsWithoutCrash) {
+    ToDoList list;
+    list.addTask("Test output");
+    // Non verifica l'output ma solo che non crashi
+    EXPECT_NO_THROW(list.showTasks());
+}
+
+
